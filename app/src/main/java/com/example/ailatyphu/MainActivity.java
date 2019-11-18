a00d81f3d500417982c1baaa6f6da74ef712701f
Le Duc Anh <57182096+ducanhberry@users.noreply.github.com>
2019-11-18 21:03:35 +0700

    V2

--- app/src/main/java/com/example/ailatyphu/MainActivity.java
+++ app/src/main/java/com/example/ailatyphu/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quan_li_tai_khoan);
     }
 }
